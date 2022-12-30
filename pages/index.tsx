2bd29382132d08ed4ec8ffb78f5cab8740103897
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-30 18:34:15 -0300

    arrumando detalhe do titulo

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
 
 export default function Home({ todos }: PostProps) {
   const [form, setForm] = useState<FormData>({ title: '', description: '', id: 0 })
+  const [title, setTitle] = useState('Criar nova tarefa')
   const router = useRouter()
 
   const refreshData = () => {
@@ -73,7 +74,7 @@ export default function Home({ todos }: PostProps) {
       <div>
         <div className="flex justify-center mt-10">
           <div className="bg-gray-50 p-8 rounded-lg">
-            <h1 className="text-center mb-4">Criar nova tarefa</h1>
+            <h1 className="text-center mb-4">{title}</h1>
             <div className="flex flex-col p-2 bg-white rounded-md">
               <input value={form.title} onChange={e => setForm({ ...form, title: e.target.value })} type="text" placeholder="Escreva o título..." className="w-full text-center py-2 mb-3 outline-none bg-slate-100" />
               <input value={form.description} onChange={e => setForm({ ...form, description: e.target.value })} type="text" placeholder="Escreva a descrição..." className="w-full text-center py-2 outline-none bg-slate-100" />
@@ -86,7 +87,10 @@ export default function Home({ todos }: PostProps) {
             <div key={todo.id} className="flex justify-center">
               <div className=" relative justify-center mt-6">
                 <div className="absolute flex top-0 right-0 p-3 space-x-1">
-                  <button onClick={() => { setForm({ title: todo.title, description: todo.description, id: todo.id }) }}>
+                  <button onClick={() => {
+                    setTitle(`Editar tarefa: ${todo.title}`);
+                    setForm({ title: todo.title, description: todo.description, id: todo.id })
+                  }}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                     </svg>
