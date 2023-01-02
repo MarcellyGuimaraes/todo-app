aee592f555efda4ba410480817cd08f62a45bb42
IsisG13 <isissouzaguimaraes@gmail.com>
2023-01-02 14:47:30 -0300

    adicionando título da página

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,10 @@
+import Head from 'next/head'
 import React from 'react'
 
 const Navbar = () => {
     return (
         <nav className="flex justify-center bg-cyan-900 p-4">
+            <Head><title>Todo App</title></Head>
             <h1 className="text-2xl font-bold text-white">My Todo App</h1>
         </nav>
     )
